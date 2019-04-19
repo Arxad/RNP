01576d828b31bb1574767443d91c744a8b6b4024
Никита Волков <nikvolkstalker@mail.ru>
2019-04-19 22:50:58 +1000

    Chtoto

---
+++ SuperRenameProject/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace SuperRenameProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.импортToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.списокДокументовToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.listBox3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 667);
+            this.panel1.TabIndex = 0;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(2, 54);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(235, 602);
+            this.listBox3.TabIndex = 0;
+            this.listBox3.SelectedValueChanged += new System.EventHandler(this.ImageVis);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 24);
+            this.button1.TabIndex = 4;
+            this.button1.TabStop = false;
+            this.button1.Text = "Импорт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "F:\\";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.импортToolStripMenuItem,
+            this.списокДокументовToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(244, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // импортToolStripMenuItem
+            // 
+            this.импортToolStripMenuItem.Name = "импортToolStripMenuItem";
+            this.импортToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.импортToolStripMenuItem.Text = "Экспорт";
+            // 
+            // списокДокументовToolStripMenuItem
+            // 
+            this.списокДокументовToolStripMenuItem.Name = "списокДокументовToolStripMenuItem";
+            this.списокДокументовToolStripMenuItem.Size = new System.Drawing.Size(128, 20);
+            this.списокДокументовToolStripMenuItem.Text = "Список документов";
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.SaveButton);
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel2.Location = new System.Drawing.Point(831, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel2.Size = new System.Drawing.Size(287, 667);
+            this.panel2.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(4, 595);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(280, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(4, 621);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(280, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.SaveButton.Location = new System.Drawing.Point(0, 644);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(287, 23);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.Сохранить_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(281, 559);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(281, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(242, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(586, 667);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1118, 667);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem импортToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem списокДокументовToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ SuperRenameProject/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+//using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SuperRenameProject
+{
+    public partial class Form1 : Form
+    {
+        public string nameIMG, imagepath;
+        public string[,] abr = new string[100, 2];
+
+
+        public void ReadFiles(string path) //Вывод списка фаилов
+        {
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                string e = Path.GetExtension(file);// Проверка расширеня фаила
+                if ((e == ".jpg") | (e == ".png") | (e == ".JPG") | (e == ".PNG"))
+                    listBox3.Items.Add(file.Remove(0, file.LastIndexOf('\\') + 1));
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists(System.IO.Path.Combine(Environment.CurrentDirectory, "SystemInfo.txt"))) // Проверка наличия SystemInfo
+            {
+                string[] line = File.ReadAllLines(System.IO.Path.Combine(Environment.CurrentDirectory, "SystemInfo.txt"), Encoding.Default);// Запись всех строк к строковый массив
+                textBox1.Text = line[0];//вывод пути импорта
+                textBox3.Text = line[1];//Вывод пути экспорта
+
+                //abr = new string[(line.Length) - 2, 2];// Создание строкового массива нужной длины
+
+                for (int i = 0; i < ((line.Length) - 2); i++)
+                {
+                    string[] result = line[i + 2].Split(new char[] { '^' });
+                    abr[i, 0] = result[0];
+                    abr[i, 1] = result[1];
+                    listBox1.Items.Add(abr[i, 0]);
+                }
+                string path = textBox1.Text;
+
+                ReadFiles(path); //Вывод списка фаилов
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)// импорт
+        {
+            listBox3.Items.Clear(); //очистка списка филов 
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = (folderBrowserDialog1.SelectedPath) + "\\";
+            }
+            string pathIn = textBox1.Text;
+            string pathOut = textBox3.Text;
+
+            ReadFiles(pathIn); //Вывод списка фаилов
+
+            string x1 = pathIn;// test
+            string x2 = pathOut;// test
+            string[] line1 = new string[] { x1, x2 };// test
+            //System.IO.File.AppendAllText("SystemInfo.txt", path); 
+            System.IO.File.WriteAllLines("SystemInfo.txt", line1, Encoding.Default);//Создание SystemInfo  
+
+        }
+
+
+        private void ImageVis(object sender, EventArgs e)
+        {
+            //textBox2.Text = Convert.ToString(listBox3.SelectedIndex);
+            //ListView.SelectedIndexCollection indexes = this.ListView1.SelectedIndeces;
+            string two = Convert.ToString(listBox3.Items[listBox3.SelectedIndex]);
+            imagepath = textBox1.Text + "\\" + two;
+            using (var memoryStream = new MemoryStream(File.ReadAllBytes(imagepath)))
+            {
+                pictureBox1.Image = new Bitmap(memoryStream);
+            }
+            //pictureBox1.Image = Image.FromFile(imagepath);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox4.Text = (textBox2.Text+"_"+Convert.ToString(abr[listBox1.SelectedIndex,1]));
+        }
+
+
+        private void Сохранить_Click(object sender, EventArgs e)
+        {
+            string extension = Path.GetExtension(imagepath);
+            string path2 = (textBox3.Text + "\\" + textBox4.Text + extension);
+
+
+
+
+            string path = textBox1.Text;
+
+            
+
+            //pictureBox1.Image = Image.FromFile(imagepath);
+            //Close();
+            File.Move(imagepath, path2);
+
+            listBox3.Items.Clear();
+            ReadFiles(path);
+            textBox4.Text = path2;
+
+            listBox3.SetSelected(0, true);
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
